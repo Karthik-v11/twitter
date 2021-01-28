9536039669ae88e6433af80f0b59be6fd24ce34f
Karthik-v11 <78066828+Karthik-v11@users.noreply.github.com>
2021-01-28 11:32:55 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -6,14 +6,16 @@ consumer_secret=os.getenv('consumer_secret')
 access_token=os.getenv('access_token')
 access_token_secret=os.getenv('access_token_secret')
 
-auth= tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
+
 api=tweepy.API(auth)
 
 import time
+
 while True:
   user=api.get_user('_karthik_V_')
   n=user.followers_count
   api.update_profile(name=f'Karthik {n} followers')
-  print(f'karthik{n}followers')
+  print(f'Karthik{n}followers')
   time.sleep(60)
