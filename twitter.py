002138d6ae4a6652540cf8b029cf0a9878c17e45
Karthik-v11 <78066828+Karthik-v11@users.noreply.github.com>
2021-01-28 12:00:22 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,13 +1,20 @@
 import tweepy
-import os
+#import os
 
-consumer_key=os.getenv('consumer_key')
-consumer_secret=os.getenv('consumer_secret')
-access_token=os.getenv('access_token')
-access_token_secret=os.getenv('access_token_secret')
+#consumer_key=os.getenv('consumer_key')
+#consumer_secret=os.getenv('consumer_secret')
+#access_token=os.getenv('access_token')
+#access_token_secret=os.getenv('access_token_secret')
 
-auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token,access_token_secret)
+#auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
+#auth.set_access_token(access_token,access_token_secret)
+CONSUMER_KEY='23qtZgYoYQHA8yTuE2jymBkma'
+CONSUMER_SECRET='50pQUQPDwI5Yvv4Cfi9bav9MTfNRQaVoiXvNglLp31Y27hZYSO'
+ACCESS_TOKEN='1135552582259159040-fa59BslRG7uIe3p81ceSF6bZXgNyi2'
+ACCESS_TOKEN_SECRET='pqwWZfn358uPdhL69tWsca467zzDk4H9sPiwsxKKITEaI'
+
+auth= tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
 
 api=tweepy.API(auth)
 
