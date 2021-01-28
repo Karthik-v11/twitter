7e4fc9dfe0f72583fb75d4e2302d01fa17a273f8
Karthik-v11 <78066828+Karthik-v11@users.noreply.github.com>
2021-01-28 10:59:57 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -10,7 +10,6 @@ auth= tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api=tweepy.API(auth)
 
-
 import time
 while True:
   user=api.get_user('_karthik_V_')
